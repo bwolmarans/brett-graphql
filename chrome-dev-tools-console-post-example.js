e19e20ec1a3886e8a97edba2519acf0b804d0b4e
Brett Wolmarans <itsbrett@gmail.com>
2025-11-26 19:59:26 +0000

    dd

--- chrome-dev-tools-console-post-example.js
+++ chrome-dev-tools-console-post-example.js
@@ -22,3 +22,26 @@ fetch('https://kong-f40972acc5us3jemn.kongcloud.dev/graphql', {
 .catch(error => {
     console.error('Error:', error);
 });
+
+fetch('https://kong-f40972acc5us3jemn.kongcloud.dev/graphql', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json', // Or 'application/x-www-form-urlencoded' depending on your API
+        'Authorization': 'Bearer YOUR_AUTH_TOKEN' // If authentication is required
+    },
+    body: JSON.stringify({ // Stringify the body for JSON content-type
+        "query": "{ greet(name: \"Brett\") }"
+    })
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json(); // Or response.text() if the response is not JSON
+})
+.then(data => {
+    console.log('Success:', data);
+})
+.catch(error => {
+    console.error('Error:', error);
+});
